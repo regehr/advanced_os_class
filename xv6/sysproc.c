bb2042d862934df69dcc15a10aefe4f94787eea2
Nicki Barneck <nbarneck@gmail.com>
2014-02-19 20:08:02 -0700

    Added sys_shmget function

--- xv6/sysproc.c
+++ xv6/sysproc.c
@@ -117,21 +117,81 @@ int sys_gettime(void)
   return 0;
 }
 
-int sys_shared(void)
+int sys_shmget(void)
 {
-  struct shared *share;
+  int addr, i, size_rup;
+  uint size, token;
+  struct proc *p;
+  char* mem;
+  pte_t* pte;
 
-  // if there's already a shared page, return now
-  if (proc->shared) {
-    return SHARED_V;
+  // Get args from user
+  if((arguint(0,&token) < 0) ||
+    (argint(1,&addr) < 0) ||
+    (arguint(2,&size) < 0)) {
+    cprintf("Failed to get user args\n");
+    return -1;
   }
 
-  share = sharedalloc();
-  if (share) {
-    proc->shared = share;
-    mappages(proc->pgdir, (char *)SHARED_V, PGSIZE, v2p(share->page), PTE_W|PTE_U);
-    return SHARED_V;
-  } else { //shared alloc failed
+  size_rup = PGROUNDUP(size);
+
+  acquire(&ptable.lock);
+
+  // check procs for matching token
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+    if((p->state != UNUSED) & (p->sh_key == token)) {
+      release(&ptable.lock);
+      goto existing;
+    }
+  }
+  release(&ptable.lock);
+
+  // if none, create new shared memory area
+  if (size > 0) {
+    for(i = 0; i < size_rup; i+= PGSIZE) {
+      mem = kalloc();
+      if(!mem) {
+        cprintf("Not enough memory\n");
+        // Deallocate memory from the process
+        deallocuvm(proc->pgdir, addr+i, addr);
+        return -1;
+      }
+
+      // Set allocated memory to 0
+      memset(mem, 0, PGSIZE);
+      mappages(proc->pgdir, (void*)addr+i, PGSIZE, v2p(mem), PTE_U|PTE_W);
+    }
+
+    proc->sh_size = size;
+    proc->sh_addr = (char*)addr;
+    proc->sh_key = token;
+
+    switchuvm(proc);
     return 0;
   }
+
+  return -1;
+  // otherwise, map shared memory together
+existing:
+  cprintf("in existing\n");
+
+  size_rup = PGROUNDUP(p->sh_size);
+
+  for(i = 0; i < size_rup; i += PGSIZE) {
+    pte = walkpgdir((pde_t*)p->pgdir, ((char*)p->sh_addr), 0);
+    if(!pte) {
+      cprintf("Not enough memory\n");
+      return -1;
+    }
+    if((*pte & PTE_P) != 0){
+      uint pa = PTE_ADDR(*pte);
+      if(!pa){
+        cprintf("Physical address was zero\n");
+        return -1;
+      }
+      mappages(proc->pgdir, (char*)addr+i, PGSIZE, pa, PTE_U|PTE_W);
+    }
+  }
+  switchuvm(proc);
+  return 0;
 }
