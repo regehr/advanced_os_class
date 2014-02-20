1dccd19b923fef404c924803b3c8210d94248558
Nicki Barneck <nbarneck@gmail.com>
2014-02-19 19:45:45 -0700

    Added ifndef call

--- xv6/spinlock.h
+++ xv6/spinlock.h
@@ -1,3 +1,5 @@
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
 // Mutual exclusion lock.
 struct spinlock {
   uint locked;       // Is the lock held?
@@ -9,3 +11,4 @@ struct spinlock {
                      // that locked the lock.
 };
 
+#endif
