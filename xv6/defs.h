1f40dcce880322e187ba3733f1d2663cbc83c95e
Nicki Barneck <nbarneck@gmail.com>
2014-02-19 19:58:38 -0700

    included walkpgdir
    Used uint* instead of pte_t* to avoid a compiler error, but they are the same thing

--- xv6/defs.h
+++ xv6/defs.h
@@ -179,6 +179,7 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 int             mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
+uint* 		    	walkpgdir(pde_t*, const void*, int);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
