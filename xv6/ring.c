cbbf1dc3f1204fbf52921a0b7b52055a343fcc84
Aric Parkinson <aric.parkinson@gmail.com>
2014-03-05 21:29:05 -0700

    Um, I think I finished it, but it won't build. I have no idea why other than a very loose notion that perhaps I've run out of space in the file system somehow? Seriously, no idea what is going on.

--- xv6/ring.c
+++ xv6/ring.c
@@ -81,7 +81,7 @@ struct ring_res ring_write_reserve(struct ring *r, int bytes)
     struct ring_res ret =
     {
         ret_size << 2,
-        BUF(r) + BUF(r)[WRITE_RES]
+        (int*)(BUF(r) + BUF(r)[WRITE_RES])
     };
 
     BUF(r)[WRITE_RES] += ret_size;
@@ -120,7 +120,7 @@ struct ring_res ring_read_reserve(struct ring *r, int bytes)
     struct ring_res ret =
     {
         ret_size << 2,
-        BUF(r) + BUF(r)[READ_RES]
+        (int*)(BUF(r) + BUF(r)[READ_RES])
     };
 
     BUF(r)[READ_RES] += ret_size;
@@ -146,10 +146,22 @@ void ring_read_notify(struct ring *r, int bytes)
 
 void ring_write(struct ring *r, void *buf, int bytes)
 {
-    // TODO
+    while (bytes > 0)
+    {
+        struct ring_res write_res = ring_write_reserve(r, bytes);
+        // ?!?!?!?!
+        // So, I was defining another function that did this in terms of integers, and suddenly
+        // mkfs was failing on building (seriously - make stopped working!) and I can't figure
+        // what could be going on. For now, we'll copy by bytes even though that's terrible. :|
+        memmove(write_res.buf, buf, write_res.size << 2);
+        bytes -= (write_res.size << 2);
+        ring_write_notify(r, write_res.size << 2);
+    }
 }
 
 void ring_read(struct ring *r, void *buf, int bytes)
 {
-    // TODO
+    struct ring_res read_res = ring_read_reserve(r, bytes);
+    memmove(buf, read_res.buf, read_res.size << 2);
+    ring_read_notify(r, read_res.size << 2);
 }
