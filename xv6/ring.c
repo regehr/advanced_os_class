bad35390d64f225ea1f6664a9ac6178ccdfd28fc
ariley <andrew@webtapper.net>
2014-03-13 17:46:40 -0600

    Started ring buffer

--- xv6/ring.c
+++ xv6/ring.c
@@ -1,48 +1,67 @@
 #include "ring.h"
 
+/* Attach Shemget to the ring? */
 struct ring *ring_attach(uint token)
 {
   return 0;
 }
 
+/* Return the entire size of the ring */
 int ring_size(uint token)
 {
-  return 0;
+  return RING_SIZE;
 }
 
+/* Dettach Shemget from the ring? */
 int ring_detach(uint token)
 {
   return 0;
 }
 
+/* Space that has been reserved and ready to be written in */
 struct ring_res ring_write_reserve(struct ring *r, int bytes)
 {
   struct ring_res ret = {0, 0};
+
+  // base cases
+  if (bytes > RING_SPACE_FREE)
+	return ret
+
+  unsigned int old = *poiner->write_reserve;
+  *pointer->write_reserve += bytes
+
+  ret->size = *pointer->write_reserve;
+  ret->buf = RING_START + (old & (RING_SIZE - 1));
   return ret;
 }
 
+/* Notify writer they have space to write in */
 void ring_write_notify(struct ring *r, int bytes)
 {
 
 }
 
+/* Space that will be read and should not be reserved for writing just yet */
 struct ring_res ring_read_reserve(struct ring *r, int bytes)
 {
   struct ring_res ret = {0, 0};
   return ret;
 }
 
+/* Notify reader they have space to read in */
 void ring_read_notify(struct ring *r, int bytes)
 {
 
 }
 
-void ring_write(struct ring *r, void *buf, int bytes)
+/* Write in space */
+int ring_write(struct ring *r, void *buf, int bytes)
 {
 
 }
 
-void ring_read(struct ring *r, void *buf, int bytes)
+/* Read in space */
+int ring_read(struct ring *r, void *buf, int bytes)
 {
 
 }
--- xv6/ring.h
+++ xv6/ring.h
@@ -3,6 +3,26 @@
 
 #include "types.h"
 
+
+#define RING_SIZE 0x0007F000
+#define RING_PAGE 0x1000
+#define RING_START 0x7FF00000
+
+/* Define our pointers */
+volatile struct  {
+  unsigned int read = 0x7FF80000;
+  unsigned int read_reserve = 0x7FF80040;
+  unsigned int write = 0x7FF80080;
+  unsigned int write_reserve = 0x7FF800C0;
+} pointer;
+
+
+#define RING_SPACE_USED (*pointer->write_reserve - *pointer->read)
+#define RING_SPACE_FREE (RING_SIZE - RING_SPACE_USED)
+
+
+
+
 struct ring {
   unsigned tok;
   void *buf;
@@ -20,7 +40,7 @@ struct ring_res ring_write_reserve(struct ring *r, int bytes);
 void ring_write_notify(struct ring *r, int bytes);
 struct ring_res ring_read_reserve(struct ring *r, int bytes);
 void ring_read_notify(struct ring *r, int bytes);
-void ring_write(struct ring *r, void *buf, int bytes);
-void ring_read(struct ring *r, void *buf, int bytes);
+int ring_write(struct ring *r, void *buf, int bytes);
+int ring_read(struct ring *r, void *buf, int bytes);
 
 #endif
